d3fe97e5946c67ca371ed275d32a2759cc2bc870
Abiria <abiria.dev@gmail.com>
2021-09-25 09:38:41 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+pub struct Post {
+    content: String,
+}
+
+impl Post {
+    pub fn new() -> DraftPost {
+        DraftPost {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+pub struct DraftPost {
+    content: String,
+}
+
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
+        }
+    }
+}
+
+pub struct PendingReviewPost {
+    content: String,
+}
+
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
+    }
+}
+
+fn main() {
+    let mut post = Post::new();
+
+    let some_text = "Awareness happens when you meet issue so balanced that whatsoever you are remaining is your music.";
+
+    post.add_text(some_text);
+
+    let post = post.request_review().approve();
+
+    assert_eq!(post.content(), some_text);
+}
